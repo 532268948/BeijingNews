0c4b656254c309418727d6e375309790bc1310f8
unknown <>
2017-04-27 17:16:51 +0800

    Splash Flash.

--- app/src/main/java/com/example/beijingnews/SplashActivity.java
+++ app/src/main/java/com/example/beijingnews/SplashActivity.java
@@ -2,12 +2,55 @@ package com.example.beijingnews;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.RotateAnimation;
+import android.view.animation.ScaleAnimation;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 public class SplashActivity extends AppCompatActivity {
+    private RelativeLayout rl_splash_root;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        rl_splash_root=(RelativeLayout)findViewById(R.id.rl_splash_root);
+        RotateAnimation ra=new RotateAnimation(0,360,RotateAnimation.RELATIVE_TO_SELF,0.5f,RotateAnimation.RELATIVE_TO_SELF,0.5f);
+
+        AlphaAnimation aa=new AlphaAnimation(0,1);
+
+        ScaleAnimation sa=new ScaleAnimation(0,1,0,1,ScaleAnimation.RELATIVE_TO_SELF,0.5f,ScaleAnimation.RELATIVE_TO_SELF,0.5f);
+
+        AnimationSet set=new AnimationSet(false);
+        set.addAnimation(ra);
+        set.addAnimation(aa);
+        set.addAnimation(sa);
+        set.setDuration(2000);
+        set.setFillAfter(true);
+
+        rl_splash_root.startAnimation(set);
+
+        set.setAnimationListener(new MyAnimationListener());
+
+    }
+    class MyAnimationListener implements Animation.AnimationListener {
+
+        @Override
+        public void onAnimationStart(Animation animation) {
+
+        }
+
+        @Override
+        public void onAnimationEnd(Animation animation) {
+            Toast.makeText(SplashActivity.this,"动画播放完成",Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onAnimationRepeat(Animation animation) {
+
+        }
     }
 }
